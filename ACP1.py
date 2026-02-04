9d367f91d748714990b155eb7d30ec9bcdbb0853
hi122290 <lukey37272@gmail.com>
2026-02-03 20:16:08 -0500

    Add files via upload

---
+++ ACP1.py
@@ -0,0 +1,10 @@
+Logan = "3/22"
+Fransisco = "5/19"
+Adam = "6/1"
+Ethan = "8/23"
+Axel = "12/19" 
+print("Logan's birthday is on: ", Logan)
+print("Fransisco's birthday is on: ", Fransisco)
+print("Adams's birthday is on: ", Adam)
+print("Ethan's birthday is on: ", Ethan)
+print("Axel's birthday is on: ", Axel)
