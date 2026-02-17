9e266010bde376f61db04d78e0f5a7c64166ccb6
hi122290 <lukey37272@gmail.com>
2026-02-16 20:14:46 -0500

    Add files via upload

---
+++ test10.py
@@ -0,0 +1,6 @@
+height=int(input("enter height"))
+age=int(input("enter age"))
+if height>= 5 and age>= 12:
+    print("you can take this ride")
+else:
+    print("you cannot take this ride")
---
+++ test11.py
@@ -0,0 +1,5 @@
+a = -10
+b = 10
+
+if a > 0 or b > 0:
+    print("Both numbers are greater than zero.")
